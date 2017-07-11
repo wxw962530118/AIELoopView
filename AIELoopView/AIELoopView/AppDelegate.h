08cde7b2def9000d6f9a4a15c243a93be8473925
精简 <13120955708@163.com>
2017-07-11 14:34:20 +0800

    [change]nothing

--- AIELoopView/AIELoopView/AppDelegate.h
+++ AIELoopView/AIELoopView/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by 王新伟 on 2017/6/21.
 //  Copyright © 2017年 王新伟. All rights reserved.
 //
-
+/////
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
